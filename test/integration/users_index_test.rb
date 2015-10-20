3153c7f6e059cad860b040186e94b1e5e1e4bd46
Joe Reich <reich.sj1@gmail.com>
2015-10-20 12:52:31 -0400

    Fixed user-index test

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -11,9 +11,9 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users = User.order(:last_name).paginate(page: 1)
     first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: full_name(user)
+      assert_select 'a[href=?]', user_path(user)
       unless user == @admin
         assert_select 'a[href=?]', user_path(user), text: 'Delete'
       end
