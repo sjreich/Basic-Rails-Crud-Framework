679db54b30d5900c83ecf1296cfb2df62d4ed44f
Joe Reich <reich.sj1@gmail.com>
2015-10-20 12:32:02 -0400

    many minor changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def main_title
+    "Main Title"
+  end
+
   def full_name(user)
     "#{user.first_name} #{user.last_name}"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-User.create!( first_name:  "Example",
-              last_name: "User",
-              email: "example@railstutorial.org",
+User.create!( first_name:  "Fake",
+              last_name: "Name",
+              email: "fake@name.com",
               password:              "password",
               password_confirmation: "password",
               admin: true)
@@ -9,7 +9,7 @@ User.create!( first_name:  "Example",
   name = Faker::Name.name.split(" ")
   first_name = name[0]
   last_name = name[1]
-  email = "example-#{n+1}@railstutorial.org"
+  email = "example-#{n+1}@fakename.com"
   password = "password"
   User.create!(first_name: first_name,
                last_name: last_name,
